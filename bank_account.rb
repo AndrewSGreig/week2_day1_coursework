d295abc9f9f095321adfb6387ca045bb8c14b105
User <user@Users-MacBook-Pro.local>
2020-01-13 14:05:34 +0000

    getters & setters

--- bank_account.rb
+++ bank_account.rb
@@ -1,35 +1,51 @@
 class BankAccount
+  # attr_reader :holder_name, :balance, :type
+  # attr_writer :holder_name, :balance, :type
+  attr_accessor :holder_name, :balance, :type
+
   def initialize(input_holder_name, input_balance, input_type)
     @holder_name = input_holder_name
     @balance = input_balance
     @type = input_type
 
-  end
+    def pay_in(value)
+      @balance += value
+    end
 
-  # GETTERS
-  def holder_name()
-    return @holder_name
+    def pay_monthly_fee()
+      if @type =="business"
+        @balance -= 50
+      elsif @type =="personal"
+        @balance -= 10
+      end
+    end
   end
+  # # GETTERS
+  # def holder_name()
+  #   return @holder_name
+  # end
+  #
+  # def balance()
+  #   return @balance
+  # end
+  #
+  # def type
+  #   return @type
+  # end
+  #
+  # #SETTERS
+  # def set_holder_name(new_holder_name)
+  #   @holder_name = new_holder_name
+  # end
+  #
+  # def set_balance(new_balance)
+  #   @balance = new_balance
+  # end
+  #
+  # def set_type(new_type)
+  #   @type = new_type
+  # end
 
-  def balance()
-    return @balance
-  end
 
-  def type
-    return @type
-  end
-
-  #SETTERS
-  def set_holder_name(new_holder_name)
-    @holder_name = new_holder_name
-  end
-
-  def set_balance(new_balance)
-    @balance = new_balance
-  end
-
-  def set_type(new_type)
-    @type = new_type
-  end
 
 end
--- specs/bank_account_spec.rb
+++ specs/bank_account_spec.rb
@@ -27,18 +27,43 @@ class TestBankAccount < MiniTest::Test
 
   #test setters
   def test_set_account_name
-    @bank_account = set_holder_name("Jordan")
+    # @bank_account.set_holder_name("Jordan")
+    @bank_account.holder_name = "Jordan"
     assert_equal("Jordan", @bank_account.holder_name)
   end
 
   def test_set_balance
-    @bank_account = set_balance(200)
-    assert_equal(200, @bank_account.balance)
+    # @bank_account.set_balance(220)
+    @bank_account.balance = 220
+    assert_equal(220, @bank_account.balance)
   end
 
   def test_set_value
-    @bank_account = set_type('personal')
+    assert_equal('business', @bank_account.type)
+    # @bank_account.set_type('personal')
+    @bank_account.type = "personal"
     assert_equal('personal', @bank_account.type)
+  end
+
+  def test_pay_into_account
+    @bank_account.pay_in(1000)
+    assert_equal(1300, @bank_account.balance)
+  end
+
+  # def test_pay_monthly_fee
+  #   @bank_account.pay_monthly_fee(20)
+  #   assert_equal(280, @bank_account.balance)
+  # end
+
+  def test_pay_monthly_fee_business_account
+    @bank_account.pay_monthly_fee()
+    assert_equal(250, @bank_account.balance)
+  end
+
+  def test_pay_monthly_fee_personal
+    @bank_account.type = "personal"
+    @bank_account.pay_monthly_fee()
+    assert_equal(290, @bank_account.balance)
   end
 
 
